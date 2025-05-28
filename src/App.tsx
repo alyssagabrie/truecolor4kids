622fbe857c4e44f857098cabfb96264b4a8b71ed
alyssagabrie <116127979+alyssagabrie@users.noreply.github.com>
2025-05-28 19:29:34 -0400

    Update Router configuration with basename

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/truecolor4kids">
       <div className="App">
         <Navbar />
         <main className="main-content">
