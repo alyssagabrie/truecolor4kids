92240304d6cb0ab74e5dbe13dde13426c9488d79
alyssagabrie <116127979+alyssagabrie@users.noreply.github.com>
2025-05-28 20:05:08 -0400

    Switch to BrowserRouter and add 404.html for clean URLs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import ColorExploreView from './pages/ColorExploreView';
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/truecolor4kids">
       <div className="App">
         <Navbar />
         <main className="main-content">
